a79a507365e7c6c1e9ed82f16ebbfd7305b261d0
Tomas Petricek <tomas@tomasp.net>
2011-07-24 17:00:56 +0100

    Adding sample implementation of join calculus for match!

---
+++ README.markdown
@@ -0,0 +1,54 @@
+F# Joinad Samples
+=================
+
+This repository contains various examples that use the `match!` extension for the F# 
+langauge. This is a prototype implementation for research purposes. 
+
+Join calculus
+-------------
+
+The `match!` extension can be used to encode computations based on _join calculus_.
+A buffer that provides two put channels (for storing strings and integers) and a 
+single get channel for retreiving values can be encoded as follows:
+
+    let putString = Channel<string>("puts")
+    let putInt = Channel<int>("puti")
+    let get = SyncChannel<string>("get")
+
+    join {
+      match! get, putString, putInt with
+      | repl, v, ? -> return react { yield repl.Reply("Echo " + v) }
+      | repl, ?, v -> return react { yield repl.Reply("Echo " + (string v)) } 
+    }
+
+The first three lines define channels. The `get` value represents a synchronous
+channel (when we call it, we need to wait for a reply). The `putInt` and `putString`
+channels represent two methods for adding values to the buffer.
+
+The join program is implemented using the `join` computation builder that defines
+primitives for _merging_ channels, _projecting_ values in the channel and for
+non-deterministic _choice_ between channels. These primitives return _alias_ channels
+that do not actually contain values - they just query the original channels for
+a value when a value is requested. 
+
+The two clauses of the `match!` expression represent two _join patterns_. In the first
+join pattern, we require a value from the `get` channel and the `putString` channel.
+When the values are available, we return a reaction. The reaction is created using
+`react` computation builder and it can yield operations to be performed. In this sample,
+the only operation is to send a reply to the caller of the `get` channel.
+
+The buffer can be called using F# asynchronous workflows as follows:
+
+    // Put 5 values to 'putString' and 5 values to 'putInt'
+    for i in 1 .. 5 do 
+      putString.Call("Hello!")
+      putInt.Call(i)
+
+    // Repeatedly call 'get' to read the next value. This is a blocking
+    // operation, so it should be done from asynchronous workflow to 
+    // avoid blocking physical threads.
+    async { 
+      while true do
+        let! repl = get.AsyncCall()
+        printfn "got: %s" repl }
+    |> Async.Start
---
+++ src/Joins/Samples.fs
@@ -0,0 +1,106 @@
+﻿// ----------------------------------------------------------------------------
+// F# Joinads Samples - Samples using joins (Samples.fs)
+// (c) Tomas Petricek, 2011, Available under Apache 2.0 license.
+// ----------------------------------------------------------------------------
+
+#if INTERACRIVE
+#load "Joins.fs"
+#else
+module Samples 
+#endif
+
+open System
+open System.Threading
+open FSharp.Joinads.Joins
+
+// ----------------------------------------------------------------------------
+// A buffer with two different put channels
+// ----------------------------------------------------------------------------
+
+let twoPutBuffer() =
+  let putString = Channel<string>("puts")
+  let putInt = Channel<int>("puti")
+  let get = SyncChannel<string>("get")
+
+  join {
+    match! get, putString, putInt with
+    | repl, v, ? -> return react { yield repl.Reply("Echo " + v) }
+    | repl, ?, v -> return react { yield repl.Reply("Echo " + (string v)) } 
+  }
+
+
+  // Put 5 string messages to the putString channel
+  async { for i in 1 .. 5 do 
+            do! Async.Sleep(1000) 
+            printfn "putting: World (#%d)" i
+            putString.Call(sprintf "World (#%d)" i) }
+  |> Async.Start
+
+  // Put 5 int messages to the putInt channel
+  async { do! Async.Sleep(2000)
+          for i in 1 .. 5 do 
+            do! Async.Sleep(1000) 
+            printfn "putting: %d" (1000 + i)
+            putInt.Call(1000 + i) }
+  |> Async.Start
+
+  // Repeatedly read messages by calling 'Get'
+  async { while true do
+            do! Async.Sleep(500)
+            printfn "reading..."
+            let! repl = get.AsyncCall()
+            printfn "got: %s" repl }
+  |> Async.Start
+
+
+// ----------------------------------------------------------------------------
+// One place buffer sample
+// ----------------------------------------------------------------------------
+
+let onePlaceBuffer() = 
+  let put = SyncChannel<string, unit>()
+  let get = SyncChannel<string>()
+  let empty = Channel<unit>()
+  let contains = Channel<string>()
+  
+  // Initially, the buffer is empty
+  empty.Call(())
+
+  join {
+    match! put, empty, get, contains with 
+    | (s, repl), (), ?, ? -> return react {
+      yield contains.Put(s)
+      yield repl.Reply() }
+    | ?, ?, repl, v -> return react {
+      yield repl.Reply(v)
+      yield empty.Put(()) } }
+
+  // Repeatedly try to put value into the buffer
+  async { do! Async.Sleep(1000)
+          for i in 0 .. 10 do
+            printfn "putting: %d" i
+            do! put.AsyncCall(string i)
+            do! Async.Sleep(500) }
+  |> Async.Start
+
+  // Repeatedly read values from the buffer and print them
+  async { while true do 
+            do! Async.Sleep(250)
+            let! v = get.AsyncCall()
+            printfn "got: %s" v }
+  |> Async.Start
+
+
+// ----------------------------------------------------------------------------
+// Main function 
+// ----------------------------------------------------------------------------
+
+[<EntryPoint>]
+let main (args) =
+  twoPutBuffer()
+  Console.ReadLine()
+  
+  onePlaceBuffer()
+  Console.ReadLine()
+  
+  0
